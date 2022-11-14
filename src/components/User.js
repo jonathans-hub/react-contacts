a8f3caa9a9e252b5eac0c3ae979a44710f87879e
Jonathan <110108852+jonathans-hub@users.noreply.github.com>
2022-11-14 08:21:20 -0400

    Second Commit

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,30 @@
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
 
-export default function User (props) {
-    return (
-      <Col md="4"style={{marginBottom: "1rem"}}>
-        <Card style={{ width: "15rem" }}>
-          <Card.Body>
-            <Card.Subtitle className="mb-2 text-muted">
-              CodeTrain User
-            </Card.Subtitle>
-            <Card.Title>{props.userInfo.name}</Card.Title>
-            <Card.Text>
-              <p>Email: {props.userInfo.email}</p>
-              <p>Gen: {props.userInfo.gen}</p>
-            </Card.Text>
-            <Card.Link href="#">Delete</Card.Link>
-            <Card.Link href="#">Edit</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-  }
+export default function User(props) {
+  const handleDelete = (e) => {
+    e.preventDefault();
+    console.log("item deleted");
+  };
+  return (
+    <Col md="4" style={{ marginBottom: "1rem" }}>
+      <Card style={{ width: "15rem" }}>
+        <Card.Body>
+          <Card.Subtitle className="mb-2 text-muted">
+            CodeTrain User
+          </Card.Subtitle>
+          <Card.Title>{props.userInfo.name}</Card.Title>
+          <Card.Text>
+           Email:{props.userInfo.email}
+          </Card.Text>
+          <Card.Text>Gen:{props.userInfo.gen}</Card.Text>
+          <Card.Link href="#">
+            <Button variant="danger" size="sm" onClick={handleDelete}>
+              Delete
+            </Button>
+          </Card.Link>
+          <Card.Link href="#">Edit</Card.Link>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
